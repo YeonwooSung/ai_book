6d8ad47c94ef139375423ede16ca2d74948ffcf7
YeonwooSung <neos960518@gmail.com>
2022-03-22 23:28:58 +0900

    Use pipeline with DistillGPT model

--- MachineLearning/DeepLearning/Frameworks/HuggingFace/src/pipelines/text_generation.py
+++ MachineLearning/DeepLearning/Frameworks/HuggingFace/src/pipelines/text_generation.py
@@ -4,3 +4,12 @@ generator = pipeline("text-generation")
 generated_texts = generator("In this course, we will teach you how to")
 
 print(generated_texts)
+
+generator = pipeline("text-generation", model="distilgpt2")
+generated_texts_distill_gpt = generator(
+    "In this course, we will teach you how to",
+    max_length=30,
+    num_return_sequences=2,
+)
+
+print(generated_texts_distill_gpt)
