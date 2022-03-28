2fdb76d16ccce21a3ab8db1b0eb651c8d4330ca1
YeonwooSung <neos960518@gmail.com>
2022-03-28 22:00:29 +0900

    LMs could have gender biases...

---
+++ MachineLearning/DeepLearning/Frameworks/HuggingFace/src/limitations/gender_bias.py
@@ -0,0 +1,8 @@
+from transformers import pipeline
+
+unmasker = pipeline("fill-mask", model="bert-base-uncased")
+result = unmasker("This man works as a [MASK].")
+print([r["token_str"] for r in result])
+
+result = unmasker("This woman works as a [MASK].")
+print([r["token_str"] for r in result])
