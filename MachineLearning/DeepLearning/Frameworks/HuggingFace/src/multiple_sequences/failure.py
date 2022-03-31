785aa8a2b5dc08d98a39ff3f6bf3f1b4c2894223
YeonwooSung <neos960518@gmail.com>
2022-03-31 23:13:09 +0900

    Add failing example

---
+++ MachineLearning/DeepLearning/Frameworks/HuggingFace/src/multiple_sequences/failure.py
@@ -0,0 +1,16 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint)
+
+sequence = "I've been waiting for a HuggingFace course my whole life."
+
+tokens = tokenizer.tokenize(sequence)
+ids = tokenizer.convert_tokens_to_ids(tokens)
+input_ids = torch.tensor(ids)
+
+
+# This line will fail.
+model(input_ids)
