0cad3f0d729c35315319a65f72a2514d0f5735f9
YeonwooSung <neos960518@gmail.com>
2022-04-02 13:48:40 +0900

    From tokenizers to models

---
+++ MachineLearning/DeepLearning/Frameworks/HuggingFace/src/models_configs_and_tokenizers/from_tokenizer_to_model.py
@@ -0,0 +1,10 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint)
+sequences = ["I've been waiting for a HuggingFace course my whole life.", "So have I!"]
+
+tokens = tokenizer(sequences, padding=True, truncation=True, return_tensors="pt")
+output = model(**tokens)
