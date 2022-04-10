9f53b24b454badc69b1b01e1bb75d629351a2487
YeonwooSung <neos960518@gmail.com>
2022-04-10 19:53:07 +0900

    Sample code for training bert_base_uncased w/ glue

---
+++ MachineLearning/DeepLearning/Frameworks/HuggingFace/src/trainer/bert_base_uncased_glue.py
@@ -0,0 +1,14 @@
+from datasets import load_dataset
+from transformers import AutoTokenizer, DataCollatorWithPadding
+
+raw_datasets = load_dataset("glue", "mrpc")
+checkpoint = "bert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+
+def tokenize_function(example):
+    return tokenizer(example["sentence1"], example["sentence2"], truncation=True)
+
+
+tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
